843350d0d583daa7d8f8f6c69ca0ae24c6708605
Mateus <lacerdam197@gmail.com>
2024-01-12 11:41:38 -0300

    Ultimas Alteracoes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,24 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
+       
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        
+        protected string Modelo;
+        protected string Imei;
+        protected int Memoria;
+        public string Numero { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero ,string modelo ,string imei ,int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
+
+        
 
         public void Ligar()
         {
@@ -21,6 +31,6 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public abstract  void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("123456789", "Nokia Modelo X", "123456789012345", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatssap");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("998971890","Iphone XS","5415578898888", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("instagram");
